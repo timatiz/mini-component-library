b598f788772f9929e167ac5a00bbeb79209fae3d
Trevloka <treflopl@gmail.com>
2021-10-10 21:36:50 +0200

    rewriten along video

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
+};
 
 const IconInput = ({
   label,
@@ -12,8 +27,62 @@ const IconInput = ({
   width = 250,
   size,
   placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+  const styles = STYLES[size];
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      <TextInput
+        style={{
+          "--width": width + "px",
+          "--fontSize": styles.fontSize + "px",
+          "--height": styles.height + "px",
+          "--border-thickness": styles.borderThickness + "px",
+        }}
+        placeholder={placeholder}
+      />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: black;
+  }
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--fontSize);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,36 +5,52 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
+};
+
 const Container = styled.div`
   width: auto;
-  background: ${COLORS.gray50};
-  border-radius: 1rem;
-  padding: ${(props) => (props.size === "large" ? "3px" : "0px")};
+  background: ${COLORS.transparentGray15};
+  border-radius: var(--radius);
+  padding: var(--padding);
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
 `;
 
 const Bar = styled.div`
-  width: ${(props) => props.value}%;
+  width: var(--width);
   background: ${COLORS.primary};
-  border-bottom-left-radius: 1rem;
-  border-top-left-radius: 1rem;
-  padding: ${({ size }) => {
-    if (size === "large") {
-      return "8px 0px";
-    } else if (size === "medium") {
-      return "6px 0px";
-    } else {
-      return "4px 0px";
-    }
-  }};
-  border-radius: ${(props) => {
-    if (props.value >= 98) {
-      return "1rem";
-    }
-  }};
+  height: var(--height);
+  border-radius: 4px 0 0 4px;
+`;
+
+const BarWrapper = styled.div`
+  /* trim off corners when the bar is near full */
+  overflow: hidden;
+  border-radius: 4px;
 `;
 
 const ProgressBar = ({ value, size }) => {
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
   return (
     <Container
       size={size}
@@ -42,8 +58,20 @@ const ProgressBar = ({ value, size }) => {
       aria-valuemin="0"
       aria-valuemax="100"
       aria-valuenow={value}
+      style={{
+        "--padding": styles.padding + "px",
+        "--radius": styles.radius + "px",
+      }}
     >
-      <Bar size={size} value={value} />
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          style={{
+            "--height": styles.height + "px",
+            "--width": value + "%",
+          }}
+        />
+      </BarWrapper>
     </Container>
   );
 };
