7f2cc901f32efdcad2e37d2c00def7bb39cefe5b
Trevloka <treflopl@gmail.com>
2021-10-10 16:08:54 +0200

    finish progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,51 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const Container = styled.div`
+  width: auto;
+  background: ${COLORS.gray50};
+  border-radius: 1rem;
+  padding: ${(props) => (props.size === "large" ? "3px" : "0px")};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const Bar = styled.div`
+  width: ${(props) => props.value}%;
+  background: ${COLORS.primary};
+  border-bottom-left-radius: 1rem;
+  border-top-left-radius: 1rem;
+  padding: ${({ size }) => {
+    if (size === "large") {
+      return "8px 0px";
+    } else if (size === "medium") {
+      return "6px 0px";
+    } else {
+      return "4px 0px";
+    }
+  }};
+  border-radius: ${(props) => {
+    if (props.value >= 98) {
+      return "1rem";
+    }
+  }};
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Container
+      size={size}
+      role="progressbar"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      aria-valuenow={value}
+    >
+      <Bar size={size} value={value} />
+    </Container>
+  );
 };
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+const Container = styled.select`
+  padding: 10px 6px;
+  width: auto;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Container value={value} onChange={onChange}>
       {children}
-    </select>
+    </Container>
   );
 };
 
